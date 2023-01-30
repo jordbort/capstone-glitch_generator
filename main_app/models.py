48f40e33fe73b855397847fddf703e57aad79bf9
jordbort <jordan.bortner@gmail.com>
2023-01-30 14:19:06 -0500

    Creating Profile and Post models, views, and URL routes

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    description = models.TextField()
+    image_url = models.CharField(max_length=255)
+    # tags = ?
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.description
+
+    class Meta:
+        # try '-created_at' for reverse chronological order
+        ordering = ['created_at']
+
+
+class Profile(models.Model):
+    social_link = models.CharField(max_length=255)
+    # highlight_color = models.CharField(max_length=7, default='#FFFFFF')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # def __str__(self):
+        # return self.user
--- main_app/views.py
+++ main_app/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from .models import *
 
 # from django.shortcuts import redirect
-# from django.urls import reverse
+from django.urls import reverse
 # from django.views import View
 from django.views.generic.base import TemplateView
-# from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 # from django.views.generic.detail import DetailView
 # from django.contrib.auth import login
 # from django.contrib.auth.forms import UserCreationForm
@@ -26,3 +26,17 @@ class Home(TemplateView):
 
 class About(TemplateView):
     template_name = 'about.html'
+
+
+class PostCreate(CreateView):
+    template_name = 'post_create.html'
+    model = Post
+    fields = ['description']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(PostCreate, self).form_valid(form)
+
+    def get_success_url(self):
+        print(self.kwargs)
+        return reverse('post_detail', kwargs={'pk': self.object.pk})
