4e5a384e4f947575f8ec69805017380a81ba4640
jordbort <jordan.bortner@gmail.com>
2023-01-30 11:50:44 -0500

    Creating templates and home URL

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import *
+
+# from django.shortcuts import redirect
+# from django.urls import reverse
+# from django.views import View
+from django.views.generic.base import TemplateView
+# from django.views.generic.edit import CreateView, UpdateView, DeleteView
+# from django.views.generic.detail import DetailView
+# from django.contrib.auth import login
+# from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.decorators import login_required
+# from django.utils.decorators import method_decorator
 
 # Create your views here.
+
+
+class Home(TemplateView):
+    template_name = 'post_index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context["playlists"] = Playlist.objects.all() <-- not "playlist" lol
+        return context
