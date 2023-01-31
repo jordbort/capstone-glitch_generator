a781ad83c5e886916cad1cf5a6f52fea42952dbb
jordbort <jordan.bortner@gmail.com>
2023-01-31 17:19:57 -0500

    Correcting routes

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,10 +6,10 @@ urlpatterns = [
     path('about/', views.About.as_view(), name='about'),
     path('post/create/', views.PostCreate.as_view(), name='post_create'),
     path('post/<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
-    path('post/<int:pk>/edit', views.PostUpdate.as_view(), name='post_update'),
+    path('post/<int:pk>/edit', views.PostUpdate.as_view(), name='post_edit'),
     path('post/<int:pk>/delete', views.PostDelete.as_view(), name='post_delete'),
     path('profile/<int:pk>/', views.ProfileDetail.as_view(), name='profile_detail'),
-    path('profile/<int:pk>/edit', views.ProfileUpdate.as_view(), name='profile_update'),
+    path('profile/<int:pk>/edit', views.ProfileUpdate.as_view(), name='profile_edit'),
     path('profile/<int:pk>/delete', views.ProfileDelete.as_view(), name='profile_delete'),
     path('accounts/register/', views.Register.as_view(), name='register'),
 ]
