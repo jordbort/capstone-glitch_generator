de5feaf188e45a22a7e2b5cbffff6de69e492e67
jordbort <jordan.bortner@gmail.com>
2023-02-01 16:08:46 -0500

    Adding deployment configuration

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn glitch_generator.wsgi
--- glitch_generator/settings.py
+++ glitch_generator/settings.py
@@ -125,3 +125,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+# Other settings above
+
+# Configure Django App for Heroku.
+import django_on_heroku
+django_on_heroku.settings(locals())
