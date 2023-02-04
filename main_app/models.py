0ededcff48439dc3f0197f97234d640b6214465e
jordbort <jordan.bortner@gmail.com>
2023-02-03 19:31:40 -0500

    Changing image url back to charfield

--- main_app/models.py
+++ main_app/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     description = models.TextField()
-    image_url = models.TextField()
+    image_url = models.CharField(max_length=255)
     # tags = ?
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
