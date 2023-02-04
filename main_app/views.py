682a7b25731a2ff299bd65c24acd5f2afa268b38
jordbort <jordan.bortner@gmail.com>
2023-02-04 16:52:04 -0500

    FINALLY implementing Post creation functionality

--- main_app/views.py
+++ main_app/views.py
@@ -4,7 +4,7 @@ from .models import *
 from django.urls import reverse
 from django.views import View
 from django.views.generic.base import TemplateView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import UpdateView, DeleteView
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
@@ -58,10 +58,7 @@ def image_upload(request):
                 region = os.getenv('S3_REGION_CODE')
                 s3.upload_fileobj(image_file, bucket, image_file.name)
                 s3_url = f'https://{bucket}.s3.{region}.amazonaws.com/{image_file.name}'
-                print('AWS S3 Bucket URL:', s3_url)
-                context = {'image_url': s3_url}
-                return redirect('post_create')
-                # return render(request, 'post_create.html', context)
+                return redirect(f'/post/new?url={s3_url}')
             except botocore.exceptions.ClientError as error:
                 print(error)
                 return redirect('post_index')
@@ -69,8 +66,7 @@ def image_upload(request):
             print('Error: No image file found')
             return redirect('post_index')
     else:
-        print('>> NOT a POST request')
-        # context = {"form": form}
+        print('NOT a POST request')
         return render(request, 'post_index.html')
 
 
@@ -88,10 +84,30 @@ class NewImage(TemplateView):
 
 
 @method_decorator(login_required, name='dispatch')
-class PostCreate(CreateView):
-    template_name = 'post_create.html'
-    model = Post
-    fields = ['description', 'image_url']
+class PostCreate(View):
+
+    def get(self, request):
+        url = request.GET.get("url")
+        context = {'post_image_url': url}
+        current_user = request.user
+        if current_user.is_authenticated:
+            context['auth_profile'] = Profile.objects.get(
+                user_id=current_user.id)
+        return render(request, 'post_create.html', context)
+
+    def post(self, request):
+        user = request.user
+        post_image_url = request.POST.get('image_url')
+        post_description = request.POST.get('description')
+        print('user:', user)
+        print('user.id:', user.id)
+        print('image_url:', post_image_url)
+        print('description:', post_description)
+        new_post = Post(user_id=user.id, image_url=post_image_url,
+                        description=post_description)
+        new_post.save()
+        print(new_post.id)
+        return redirect('post_index')
 
     def add_image(request, user):
         return redirect('post_index')
@@ -99,22 +115,11 @@ class PostCreate(CreateView):
     def form_valid(self, form):
         print('>> form valid is running')
         form.instance.user = self.request.user
-        # print('self:', self)
-        # print('form:', form)
-        # return redirect('post_create')
         return super(PostCreate, self).form_valid(form)
 
     def get_success_url(self):
         print(self.kwargs)
         return reverse('post_index')
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        current_user = self.request.user
-        if current_user.is_authenticated:
-            context['auth_profile'] = Profile.objects.get(
-                user_id=current_user.id)
-        return context
 
 
 @method_decorator(login_required, name='dispatch')
@@ -186,18 +191,11 @@ class ProfileUpdate(UpdateView):
     fields = ['social_link']
     template_name = "profile_edit.html"
 
-    # def get(self, request):
-    #     form = UserCreationForm()
-    #     context = {"form": form}
-    # return render(request, "registration/register.html", context)
-
     def get_success_url(self):
         return reverse('profile_detail', kwargs={'pk': self.object.pk})
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # form = UserCreationForm()
-        # context['form'] = form
         current_user = self.request.user
         if current_user.is_authenticated:
             context['auth_profile'] = Profile.objects.get(
@@ -232,7 +230,6 @@ class Register(View):
             user = form.save()
             Profile(user_id=user.id).save()
             login(request, user)
-            # return redirect("post_index")
             current_user = self.request.user
             new_profile = Profile.objects.get(user_id=current_user.id)
             print('*** current_user.id:', current_user.id)
